47ef26d338b22d5d6c63056965555863d2dae937
Ricardo Montero <Ricardo.Montero@bd.com>
2022-03-29 18:51:45 -0600

    Populate Categories and Products table migrations

---
+++ TCD/TCD.Data/Migrations/20220329215039_PopulateCategories.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TCD.Data.Migrations
+{
+    public partial class PopulateCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.InsertData(table: "Categories",
+           columns: new string[] {"Name", "Description" },
+           values: new object[,]
+           {                
+                {"Frutas","Frutas"},
+                {"Lácteos","Lácteos"},
+                {"Abarrotes","Abarrotes"},
+                {"Cuidado Personal","Cuidado Personal"},
+                {"Granos","Granos"}
+            }
+           );
+
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ TCD/TCD.Data/Migrations/20220329222013_PopulateProducts.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TCD.Data.Migrations
+{
+    public partial class PopulateProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(table: "Products",
+           columns: new string[] { "Name", "Description", "CategoryId" },
+           values: new object[,]
+           {
+                {"Manzana Gala","Manzana Gala",1},
+                {"Platano Portalimón","Platano Portalimón",1},
+                {"Mandarina","Mandarina",1},
+                {"Durazno","Durazno",1},
+                {"Naranja Valencia","Naranja Valencia",1},
+                {"Uva Roja","Uva Roja",1},
+                {"Uva Verde","Uva Verde",1},
+                {"Manzana Verde","Manzana Verde",1}
+            }
+           );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
